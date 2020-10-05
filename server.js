0793c035ba3139750675116e98d4c37adf47777e
Brandon Ruffridge <bruffridge@nasa.gov>
2020-10-05 12:38:47 -0400

    works

--- server.js
+++ server.js
@@ -34,22 +34,51 @@ app.get('/v1/search', (req, res) => {
   // ?q=1 where 1 is a function id.
   // get all wikipedia article ids that have that function id assigned.
   // use wikijs to query the wikipedia api and return article titles and summaries to display on the client.
-  
-  var articles = [];
-  var promises = [];
-  // Fixes an error: https://github.com/dijs/wiki/issues/136
-  var headers = { headers: { 'User-Agent': 'server.js (https://github.com/nasa/petal-api; bruffridge@nasa.gov) wiki.js' } }
 
-  wiki(headers).pagesInCategory('Category:American_sparrows').then( pageTitles => {
-    for (var pageTitle of pageTitles) {
-      promises.push(wiki(headers).page(pageTitle).then(page => {
-        articles.push(page);
-      }).catch(error => console.log('1 ' + error)));
+  var headers = { headers: { 'User-Agent': 'server.js (https://github.com/nasa/petal-api; bruffridge@nasa.gov) wiki.js' } }
+  var tempPages;
+  var articles = [];
+
+  wiki(headers).pagesInCategory('Category:American_sparrows')
+  .then(pageTitles =>
+    getPages(pageTitles)
+  )
+  .then(pages => {
+    tempPages = pages;
+    return getSummaries(pages);
+  })
+  .then(summaries => {
+    var i = 0;
+    for (var page of tempPages) {
+      var article = {
+        id: page.raw.pageid,
+        url: page.raw.fullurl,
+        title: page.raw.title,
+        summary: summaries[i]
+      }
+      articles.push(article);
+      i++;
     }
-    Promise.all(promises).then(() => {
-      res.json(articles);
-    }).catch(error => console.log('2 ' + error));
-  }).catch(error => console.log('3 ' + error));
+    res.json(articles);
+  })
+  .catch(error => console.log(error));
+
+  function getPages(pageTitles) {
+    var promises = [];
+    //var articles = [];
+    for (var pageTitle of pageTitles) {
+      promises.push(wiki(headers).page(pageTitle));
+    }
+    return Promise.all(promises);
+  }
+
+  function getSummaries(pages) {
+    var promises = [];
+    for (var page of pages) {
+      promises.push(page.summary());
+    }
+    return Promise.all(promises);
+  }
 });
 
 app.listen(PORT, HOST);
